7d8a4e80de818feaf8bfd4022107172bdae5a6fa
vladn255 <nesvitvlad@yandex.ru>
2021-03-05 11:59:26 +0400

    9. Контроль и ограничения (часть 2)

--- src/store/action.js
+++ src/store/action.js
@@ -2,23 +2,35 @@ import {getFilteredPlaceCards} from "../utils.js";
 import initialPlaceCards from "../mocks/offers.js";
 
 const ActionType = {
-  CHANGE_CITY: `main/changeCity`,
-  SET_PLACE_CARDS: `main/setPlaceCards`
+  CHANGE_CITY: `locationItem/changeCity`,
+  SET_PLACE_CARDS: `main/setPlaceCards`,
+  SET_ACTIVE_CARD_ID: `placesList/setActiveCardId`
 };
 
 const ActionCreator = {
-  changeCity: (city) => ({
-    type: ActionType.CHANGE_CITY,
-    payload: city
-  }),
-
-  setPlaceCards: (city) => {
+  changeCity: (city) => {
     const placeCards = getFilteredPlaceCards(initialPlaceCards, city);
     return {
-      type: ActionType.SET_PLACE_CARDS,
-      payload: placeCards
+      type: ActionType.CHANGE_CITY,
+      payload: {
+        city,
+        placeCards
+      }
     };
-  }
+  },
+
+  setPlaceCards: (cards) => {
+
+    return {
+      type: ActionType.SET_PLACE_CARDS,
+      payload: cards
+    };
+  },
+
+  setActiveCard: (cardId) => ({
+    type: ActionType.SET_ACTIVE_CARD_ID,
+    payload: cardId
+  })
 };
 
 export {
